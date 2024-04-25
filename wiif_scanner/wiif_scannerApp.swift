88e19f0a62347b598724a12dba3d82f9983c2df8
Tim Camise <tcamise@gopro.com>
2024-04-25 10:08:26 -0700

    Initial Commit

---
+++ wiif_scanner/wiif_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiif_scannerApp.swift
+//  wiif_scanner
+//
+//  Created by Tim Camise on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct wiif_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
